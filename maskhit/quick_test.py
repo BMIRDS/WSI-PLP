61c5b127925d2bd5bc4faa9f1478ea1c74a2169e
f005dg9 <f005dg9@teton.dartmouth.edu>
2024-01-15 15:41:52 -0500

    added docstring and using pathlib

--- maskhit/quick_test.py
+++ maskhit/quick_test.py
@@ -1,21 +1,27 @@
+'''
+This script is called by the `cross_validation.py` script.
+By parsing log files, it runs the train script in test mode
+resuming a model checkpoint that was trained either by
+a separate invocation of the train script or from the batch_train
+function in the `cross_validation.py` script.
+'''
+
 import sys
 import os
 import glob
-import re
-from maskhit.trainer.fitter import HybridFitter
-from maskhit.trainer.losses import FlexLoss
-from options.train_options import TrainOptions
 from utils.config import Config
+from pathlib import Path
 
 study = sys.argv[1]
 timestr = sys.argv[2]
 timestr_new = sys.argv[3]
 
+# the first three arguments passed from `cross_validation.py`
 print(f"{study}, {timestr}, {timestr_new}")
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+current_directory = Path(__file__).parent
+os.chdir(current_directory)
 files = glob.glob(f'logs/{study}/{timestr}-*.log')
-# files = glob.glob(f'logs/{study}/{timestr}_*.log')
 files = [x for x in files if not 'test' in x]
 files.sort()
 
@@ -24,19 +30,24 @@ for log_file in files:
     print(log_file)
 
 for i, log_file in enumerate(files):
+    log_file_path = Path(log_file)
     with open(log_file, 'r') as f:
-        org_cmd = f.readline().rstrip()
+        org_cmd = f.readline().rstrip() # getting original arguments from meta file
 
     org_cmd = org_cmd.replace("Argument all_arguments:", '').replace("'", "")
     if "num-patches" in " ".join(sys.argv[4:]):
         org_cmd = org_cmd.replace(" --sample-all", "")
-    ckp = os.path.basename(log_file.replace('_meta.log', ''))
-    
+    ckp = log_file_path.stem.replace('_meta', '')
+
+    # filter out the original timestr
+    if '--timestr=2023_5_30_new' in org_cmd:
+        org_cmd = org_cmd.replace("--timestr=2023_5_30_new", "")
+
+    timestr_new += '-test'
     new_cmd = ' '.join([
         'python train.py', org_cmd,
-        f' --resume={ckp} --mode=test --test-type=test --resume-epoch=BEST --timestr={timestr_new} --analyze-predictions'
-    ] + sys.argv[5:])
+        f' --mode=test --test-type=test --resume-epoch=BEST --timestr={timestr_new}'
+    ])
 
-    
     print(f"executing following new_cmd: {new_cmd}")
     os.system(new_cmd)
